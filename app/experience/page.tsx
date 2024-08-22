39a146a86c97479abbad98c03cc683a18e318809
aeron_caponpon <aeroncaponpon.01@gmail.com>
2024-08-22 17:28:32 +0800

    minor spacing adjustments

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -8,7 +8,7 @@ const Experience = () => {
       id="projects"
       className="flex flex-col justify-center items-center h-screen w-screen"
     >
-      <div className="pt-8 w-full max-w-screen-lg text-center sm:text-left">
+      <div className="pt-8 w-full max-w-screen-lg px-6 text-center sm:text-left">
         <p className="text-5xl font-bold sm:text-6xl mb-8">Experience</p>
         <ExperienceCards />
       </div>
