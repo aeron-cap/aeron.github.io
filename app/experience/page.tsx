1205a98f86089e57937d31151b8aefc7de19e526
aeron_caponpon <aeroncaponpon.01@gmail.com>
2024-08-22 16:48:26 +0800

    modified experience and experience cards for responsiveness

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,11 +4,12 @@ import ExperienceCards from "../components/ExperienceCards";
 
 const Experience = () => {
   return (
-    <main id="experience" className="flex flex-col h-auto w-screen px-40">
-      <div className="pt-16 pl-24">
-        <p className="text-6xl text-left font-bold">Experience</p>
-      </div>
-      <div className="pt-8 pl-24">
+    <main
+      id="experience"
+      className="flex flex-col justify-center items-center h-screen w-screen"
+    >
+      <div className="pt-8 w-full max-w-screen-lg px-6 sm:px-20 text-center sm:text-left">
+        <p className="text-5xl px-4 font-bold sm:text-6xl mb-8">Experience</p>
         <ExperienceCards />
       </div>
     </main>
