b70c995b93df93a3f7d17f13329768c1f4744d69
aeron_caponpon <aeroncaponpon.01@gmail.com>
2024-08-28 21:16:36 +0800

    added speed insights and analytics to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +23,8 @@ export default function RootLayout({
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       ></meta>
+      <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   );
