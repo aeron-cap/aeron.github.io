1ca6d4d91a3c486bb10ed27d332b3c84cec55cc5
aeron_caponpon <aeroncaponpon.01@gmail.com>
2024-08-28 21:10:37 +0800

    added speed insights and analytics to the main app

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,14 @@ import Projects from "./projects/page";
 import Spotlight, { SpotlightCard } from "./components/spotlight";
 import Footer from "./footer/page";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Home() {
   return (
     <div className="select-none flex">
+      <Analytics />
+      <SpeedInsights />
       <div className="flex flex-col">
         <Spotlight>
           <SpotlightCard>
